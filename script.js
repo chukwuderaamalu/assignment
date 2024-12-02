54efb099ac8805088ffc563215e5e44a59aaa976
Chukwudera Amalu <chukwuderaamalu@gmail.com>
2024-12-02 16:43:18 +0100

    assignment1

---
+++ script.js
@@ -0,0 +1,30 @@
+let button = document.getElementById('bbn')
+
+let p = document.getElementById('nap')
+
+let balloon  = document.getElementById('vvs')
+
+
+
+
+
+
+
+
+
+function sacVas () {
+    if(button.innerText = "increase") {
+        balloon.style.backgroundColor = "green"
+        balloon.style.width = "350px"
+        balloon.style.height = "350px"
+        button.innerText = "deflate"
+    }
+else  {
+    button.innerText ="increase"
+    balloon.style.backgroundColor = "black"
+    balloon.style.width = "50px"
+    balloon.style.height = "50px"
+
+};  
+    
+}
